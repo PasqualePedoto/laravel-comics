126b2e8ffc00233462c8d607645be2e0b24c7a7a
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-15 16:44:40 +0200

    Added header

---
+++ config/header_links.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    ["name" => "CHARACTERS"],
+    ["name" => "COMICS"],
+    ["name" => "MOVIES"],
+    ["name" => "TV"],
+    ["name" => "GAMES"],
+    ["name" => "COLLECTIBLES"],
+    ["name" => "VIDEOS"],
+    ["name" => "FANS"],
+    ["name" => "NEWS"],
+    ["name" => "SHOP"]
+];
+
+?>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,71 @@
+@php
+    $header_links = config('header_links');
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>{{ env('APP_NAME') }} | Homepage</title>
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/ddb740f05b.js" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <p>Siamo in home</p>
+    <!-- HEADER -->
+    <header>
+        <!-- TOP HEADER -->
+        <div id="bottom-header">
+            <div class="container">
+                <div class="content-bottom-header">
+                    <a href="#">
+                        <div class="content-bh">DC POWER VISA</div>
+                    </a>
+                    <a href="#">
+                        <div class="content-bh">ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- BOTTOM HEADER -->
+        <div id="top-header">
+            <div class="container">
+                <div class="content-top-header">
+                    <!-- Logo -->
+                    <a href="{{ url('/') }}">
+                        <figure>
+                            <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+                        </figure>
+                    </a>
+                    <!-- Links -->
+                    <div class="h-100">
+                        <ul>
+                            @foreach($header_links as $link)
+                                <li>
+                                    <a href="#">{{ $link['name'] }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <!-- Search-bar -->
+                    <div>
+                        <form action="">
+                            <div class="input-block">
+                                <input type="text" placeholder="Search">
+                                <div><i class="fa-solid fa-magnifying-glass"></i></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
