54de4351195fe178c81a331203e8688feb2b3d35
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-16 09:09:42 +0200

    Added all files for routing

--- resources/views/pages/actioncomics.blade.php
+++ resources/views/pages/actioncomics.blade.php
@@ -25,25 +25,7 @@
     @include('layouts.jumbo')
 
     <!-- Main -->
-    <main>
-        <div class="container">
-            <div class="row">
-                @foreach($comics as $comic)
-                    <div class="col">
-                        <a href="{{ route($comic['link']) }}">
-                            <figure>
-                                <img src="{{ $comic['thumb'] }}" alt="">
-                            </figure>
-                            <div class="card-title">{{ $comic['series'] }}</div>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="button-in-main">
-                <a href="#">LOAD MORE</a>
-            </div>
-        </div>
-    </main>
+    @include('layouts.main_action')
 
     <!-- FOOTER -->
     @include('layouts.footer')
